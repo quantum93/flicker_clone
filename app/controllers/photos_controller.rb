31fd488dc46ddbd5fd0857e115877ec4d7991f1f
Student <example@example.com>
2019-08-14 14:34:18 -0700

    This fixes bugs for photo control.

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -6,49 +6,61 @@ class PhotosController < ApplicationController
   end
 
   def create
-  @user = User.find(params[:user_id])
-  if @user.email == current_user.email
-    @photo = @user.photos.new(photo_params)
-    # binding.pry
-    if @photo.save
-      redirect_to user_path(@user)
+    @user = User.find(params[:user_id])
+    if @user.email == current_user.email
+      @photo = @user.photos.new(photo_params)
+      # binding.pry
+      if @photo.save
+        redirect_to user_path(@user)
+      else
+        render :new
+      end
     else
-      render :new
+      flash[:notice] = "Sorry, you are not authorized!"
+      redirect_to user_path(@user)
     end
-  else
-    flash[:notice] = "Sorry, you are not authorized!"
-    redirect_to user_path(@user)
   end
-end
 
-def show
-  @user = User.find(params[:user_id])
-  @photo = Photo.find(params[:id])
-  render :show
-end
-
-def edit
-  @user = User.find(params[:user_id])
-  @photo = Photo.find(params[:id])
-  render :edit
-end
-
-def update
-  @photo = Photo.find(params[:id])
-  if @photo.update(photo_params)
-    redirect_to user_path(@photo.user)
-  else
-    render :edit
+  def show
+    @user = User.find(params[:user_id])
+    @photo = Photo.find(params[:id])
+    render :show
   end
-end
 
-def destroy
-  @photo = Photo.find(params[:id])
-  @photo.destroy
-  redirect_to user_path(@photo.user)
-end
+  def edit
+    @user = User.find(params[:user_id])
+    @photo = Photo.find(params[:id])
+    if @user.email == current_user.email
+      @photo = Photo.find(params[:id])
+      render :edit
+    else
+      flash[:notice] = "Sorry, you are not authorized!"
+      redirect_to user_path(@photo.user)
+    end
+  end
 
-private
+  def update
+    @photo = Photo.find(params[:id])
+    if @photo.update(photo_params)
+      redirect_to user_path(@photo.user)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:user_id])
+    @photo = Photo.find(params[:id])
+    if @user.email == current_user.email
+      @photo.destroy
+      redirect_to user_path(@photo.user)
+    else
+      flash[:notice] = "Sorry, you are not authorized!"
+      redirect_to user_path(@photo.user)
+    end
+  end
+
+  private
   def photo_params
     params.require(:photo).permit(:title, :image)
   end
